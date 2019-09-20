8d93ad79a42625aa864a2f22360c00eb7aee1c68
DESKTOP-REL0A72\cyh <cyh2175@foxmail.com>
2019-09-20 18:49:57 +0800

    巡河页部分实现

---
+++ src/views/modals/addRiver.vue
@@ -0,0 +1,31 @@
+<template>
+    <a-modal title="河道查询" :visible="visible" @ok="handleOk" :confirmLoading="confirmLoading" @cancel="handleCancel">
+        <a-input-search placeholder="请输入河道名称" @search="searchRiver"/>
+    </a-modal>
+</template>
+<script>
+export default {
+    name:'addRiver',
+    data(){
+        return{
+            visible:false,
+            confirmLoading:false
+        }
+    },
+    methods:{
+        searchRiver(val){
+            console.log(val);
+        },
+        handleOk(e){
+            this.confirmLoading = true;
+            setTimeout(() => {
+                this.visible = false;
+                this.confirmLoading = false;
+            },2000);
+        },
+        handleCancel(e){
+            this.visible = false;
+        }
+    }
+}
+</script>
