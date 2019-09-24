adf35232668da1207448729264b8487cc5355354
DESKTOP-REL0A72\cyh <cyh2175@foxmail.com>
2019-09-24 18:54:08 +0800

    完善巡河主界面内容，增加调查点搜索弹框

--- src/views/dashboard/Analysis.vue
+++ src/views/dashboard/Analysis.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="main-content">
     <div class="left-info">
-      <img src="../../assets/map.jpg" style="width:100%;height: calc(100vh - 66px);">
+      <!-- <img src="../../assets/map.jpg" style="width:100%;height: calc(100vh - 66px);"> -->
+      <div id="map" ref="worldMap"></div>
       <div class="left-date">
         <a-date-picker @change="selectData" />
       </div>
@@ -15,60 +16,73 @@
         </div>
       </div>
       <search-river ref="selectPatrol" class="riverSearchModal"></search-river>
+      <add-survey ref="addSurvey" class="riverSearchModal"></add-survey>
     </div>
     <div class="right-info">
-      <a-card style="position:relative;height:100%;overflow-y: auto;" :tabList="planList" :activeTabKey="noTitleKey" @tabChange="key => onTabChange(key,'noTitleKey')">
-        <div v-if="noTitleKey === 'addPlan'">
-          <div v-if="ishidden == 1">
-            <a-row type="flex" justify="center">
-              <a-col :span="8">
-                <a-button style="padding:0 22px;" @click="addRiverBtn">添加河道</a-button>
-              </a-col>
-              <a-col :span="8" :offset="3">
-                <a-button>添加调查点</a-button>
-              </a-col>
-            </a-row>
-            <div class="riverInfo">
-              <div class="river_info">
-                <span>黄浦江</span>
-                <a-select defaultValue="" @change="handleChange" style="width:180px;margin-left:20px;">
-                  <a-select-option value="jack">Jack</a-select-option>
-                  <a-select-option value="lucy">Lucy</a-select-option>
-                  <a-select-option value="disabled" disabled>Disabled</a-select-option>
-                </a-select>
-                <a-button shape="circle" icon="close" style="font-size:8px;margin-left:15px;"></a-button>
+      <div class="right-body">
+        <a-card :tabList="planList" :activeTabKey="noTitleKey" @tabChange="key => onTabChange(key,'noTitleKey')">
+          <div v-if="noTitleKey === 'addPlan'">
+            <!-- 判断显示内容 -->
+            <div v-if="ishidden == 1">
+              <a-row type="flex" justify="center" style="margin-bottom:15px;">
+                <a-col :span="8">
+                  <a-button style="padding:0 22px;" @click="addRiverBtn">添加河道</a-button>
+                </a-col>
+                <a-col :span="8" :offset="3">
+                  <a-button @click="$refs.addSurvey.show()">添加调查点</a-button>
+                </a-col>
+              </a-row>
+              <div class="riverInfo">
+                <div class="river_info">
+                  <span>黄浦江</span>
+                  <a-select defaultValue="" @change="handleChange" style="width:180px;margin-left:20px;">
+                    <a-select-option value="jack">Jack</a-select-option>
+                    <a-select-option value="lucy">Lucy</a-select-option>
+                    <a-select-option value="disabled" disabled>Disabled</a-select-option>
+                  </a-select>
+                  <a-button shape="circle" icon="close" style="font-size:8px;margin-left:15px;"></a-button>
+                </div>
+                <a-tree checkable @expand="onExpand" :expandedKeys="expandedKeys" :autoExpandParent="autoExpandParent"
+                    v-model="checkedKeys" @select="onSelect" :selectedKeys="selectedKeys" :treeData="treeData">
+                </a-tree>
+                <a-button class="addTask_btn" @click="addTaskBtn" @addPlanInfo="clearBtn">追加任务</a-button> 
+                <add-task ref="addTask"></add-task>
               </div>
-              <a-tree checkable @expand="onExpand" :expandedKeys="expandedKeys" :autoExpandParent="autoExpandParent"
-                  v-model="checkedKeys" @select="onSelect" :selectedKeys="selectedKeys" :treeData="treeData">
-              </a-tree>
-              <a-button class="addTask_btn" @click="addTaskBtn" @addPlanInfo="clearBtn">追加任务</a-button> 
-              <add-task ref="addTask"></add-task>
+              <div class="riverInfo">
+                <div class="river_info">
+                  <span>专向调查点</span>
+                  <a-button shape="circle" icon="close" style="font-size:8px;margin-left:185px;"></a-button>
+                </div>
+                <a-tree checkable @expand="onExpand" :expandedKeys="expandedKeys" :autoExpandParent="autoExpandParent"
+                    v-model="checkedKeys" @select="onSelect" :selectedKeys="selectedKeys" :treeData="treeData">
+                </a-tree>
+                <a-button class="addTask_btn">追加任务</a-button>
+              </div>   
+            </div> 
+            <div v-if="ishidden == 2" @showPlan="getHidden">
+              <creat-group ref="creatGroup"></creat-group>
             </div>
-            <div class="riverInfo">
-              <div class="river_info">
-                <span>专向调查点</span>
-                <a-button shape="circle" icon="close" style="font-size:8px;margin-left:185px;"></a-button>
-              </div>
-              <a-tree checkable @expand="onExpand" :expandedKeys="expandedKeys" :autoExpandParent="autoExpandParent"
-                  v-model="checkedKeys" @select="onSelect" :selectedKeys="selectedKeys" :treeData="treeData">
-              </a-tree>
-              <a-button class="addTask_btn">追加任务</a-button>
+            <div v-if="ishidden == 3" >
+              <plan-list ref="planList"></plan-list>
             </div>
-            <div class="addPlan_foot">
-              <span @click="newPlan_btn">生成计划</span>
-              <span>加入已有计划</span>
-            </div>
+            <!-- <div v-if="ishidden == $refs.creatGroup.nextBtn()">
+              新建计划
+            </div> -->
           </div>
-          <div v-if="ishidden == 2">
-            <creat-group ref="creatGroup"></creat-group>
-          </div>
-          <!-- <div v-if="ishidden == $refs.creatGroup.nextBtn()">
-            新建计划
-          </div> -->
-        </div>
-        <div v-if="noTitleKey === 'nowPlan'">今日计划</div>
-      </a-card>
+          <div v-if="noTitleKey === 'nowPlan'">今日计划</div>
+        </a-card>
+        <div style="clear:both;"></div>
+      </div>
+      <!-- 底部 -->
+      <div class="addPlan_foot">
+        <div v-if="ishidden == 1">
+          <span @click="newPlan_btn">生成计划</span>
+          <span>加入已有计划</span>
+        </div>  
+      </div>
+      <div style="clear:both;"></div>
     </div>
+    <div style="clear:both;"></div>
   </div>
 </template>
 
@@ -77,6 +91,8 @@ import '../../assets/css/analysis.less'
 import searchRiver from '../modals/searchRiver'
 import addTask from '../modals/addTask'
 import creatGroup from '../modals/creatGroup'
+import planList from '../modals/planList'
+import addSurvey from '../modals/addSurvey'
 
 const treeData = [
   {
@@ -126,7 +142,9 @@ export default {
   components: {
     searchRiver,
     addTask,
-    creatGroup
+    creatGroup,
+    planList,
+    addSurvey
   },
   data() {
     return {
@@ -149,6 +167,10 @@ export default {
       ishidden: 1,
       checkedPlan:[],
       cBtn:true,
+      // 地图对象
+      map: {},
+      // 地图节点对象（里面含节点对象、区域对象、任务弹窗对象）
+      mapPoint: new Map(),
       patrolPlanInfo: [
         {
           title: '黄浦江',
@@ -185,7 +207,36 @@ export default {
       console.log('onCheck', val)
     }
   },
+  mounted() {
+    this.initCruisePlan()
+  },
   methods: {
+    initCruisePlan() {
+      const that = this
+      //初始化地图控件
+      let zoom = 14
+      that.map = new T.Map('map')
+      that.map.centerAndZoom(new T.LngLat(121.495505, 31.21098), zoom)
+      // this.map.TileLayerOptions({zIndex: 1});
+
+      // 初始化天气插件
+      /*        let a = d.getElementById('weather-float-he')
+        if (a) {
+          a.parentNode.removeChild(a)
+        }
+        a = d.createElement('div')
+        a.id = 'weather-float-he'
+        let b = d.getElementsByTagName('body')[0]
+        b.appendChild(a);
+        let c = d.createElement('link')
+        c.rel = 'stylesheet'
+        c.href = 'https://apip.weatherdt.com/float/static/css/tqw_widget_float.css?v=0101'
+        let s = d.createElement('script')
+        s.src = 'https://apip.weatherdt.com/float/static/js/tqw_widget_float.js?v=0101'
+        let sn = d.getElementsByTagName('script')[0]
+        sn.parentNode.insertBefore(c, sn)
+        sn.parentNode.insertBefore(s, sn);*/
+    },
     //选中添加河道或今日计划面板
     onTabChange(key, type) {
       console.log(key, type)
@@ -226,7 +277,11 @@ export default {
     },
     //生成计划
     newPlan_btn() {
-      this.ishidden = 2
+      this.ishidden = 3
+    },
+    getHidden(val){
+      console.log(val);
+      this.ishidden = val;
     }
   }
 }
