317d581bd77d2c2b6828a27d25c8d9af33302b1c
truthiswong <1457368987@qq.com>
2019-09-25 20:48:37 +0800

    修改logo

--- src/components/tools/Logo.vue
+++ src/components/tools/Logo.vue
@@ -1,19 +1,21 @@
 <template>
-  <div class="logo">
+  <div class="logo" title="河流监管系统">
     <router-link :to="{name:'dashboard'}">
-      <LogoSvg alt="logo" />
+      <!-- <LogoSvg alt="logo" /> -->
+      <img src="../../assets/logo.png" alt="logo">
       <h1 v-if="showTitle">{{ title }}</h1>
     </router-link>
   </div>
 </template>
 
 <script>
-import LogoSvg from '@/assets/logo.svg?inline'
+// import LogoSvg from '@/assets/logo.svg?inline'
+// import LogoSvg from '@/assets/logo.png'
 
 export default {
   name: 'Logo',
   components: {
-    LogoSvg
+    // LogoSvg
   },
   props: {
     title: {
