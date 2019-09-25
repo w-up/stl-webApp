a49e1f647f25ca70cd8cec99fcc4717372cf31de
txian <13646446594@2980.com>
2019-09-25 17:47:03 +0800

    督办单数据

--- src/views/settings/SupervisoryUnitsManage.vue
+++ src/views/settings/SupervisoryUnitsManage.vue
@@ -68,14 +68,14 @@
 
     <div class="table-operator">
       <a-button type="primary" icon="plus" @click="$refs.createModal.add()">添加</a-button>
-      <a-button type="dashed" @click="tableOption">{{ optionAlertShow && '关闭' || '开启' }} 多选</a-button>
+      <!-- <a-button type="dashed" @click="tableOption">{{ optionAlertShow && '关闭' || '开启' }} 多选</a-button>
       <a-dropdown v-action:edit v-if="selectedRowKeys.length > 0">
         <a-menu slot="overlay">
           <a-menu-item key="1">
             <a-icon type="delete" />删除
-          </a-menu-item>
+          </a-menu-item> -->
           <!-- lock | unlock -->
-          <a-menu-item key="2">
+          <!-- <a-menu-item key="2">
             <a-icon type="lock" />锁定
           </a-menu-item>
         </a-menu>
@@ -83,35 +83,28 @@
           批量操作
           <a-icon type="down" />
         </a-button>
-      </a-dropdown>
+      </a-dropdown> -->
     </div>
 
-    <s-table
-      ref="table"
-      size="default"
-      rowKey="key"
-      :columns="columns"
-      :data="loadData"
-      :alert="options.alert"
-      :rowSelection="options.rowSelection"
-      showPagination="auto"
-    >
+    <a-table :columns="columns" :dataSource="loadData" bordered>
+    <!-- :alert="options.alert"
+      :rowSelection="options.rowSelection" -->
       <span slot="serial" slot-scope="text, record, index">{{ index + 1 }}</span>
-      <span slot="status" slot-scope="text">
+      <!-- <span slot="status" slot-scope="text">
         <a-badge :status="text | statusTypeFilter" :text="text | statusFilter" />
       </span>
       <span slot="description" slot-scope="text">
         <ellipsis :length="4" tooltip>{{ text }}</ellipsis>
-      </span>
+      </span> -->
 
-      <span slot="action" slot-scope="text, record">
+      <span slot="action" slot-scope="record">
         <template>
-          <a @click="handleEdit(record)">编辑</a>
+          <a >编辑</a>
           <a-divider type="vertical" />
-          <a @click="handleSub(record)">删除</a>
+          <a >删除</a>
         </template>
       </span>
-    </s-table>
+    </a-table>
     <create-form ref="createModal" @ok="handleOk" />
     <step-by-step-modal ref="modal" @ok="handleOk" />
   </a-card>
@@ -172,19 +165,14 @@ export default {
         {
           title: '所属街道',
           dataIndex: 'description',
-          scopedSlots: { customRender: 'description' }
         },
         {
           title: '所属河道',
           dataIndex: 'callNo',
-          sorter: true,
-          needTotal: true,
-          customRender: text => text + ' 次'
         },
         {
           title: '风险源类型',
           dataIndex: 'status',
-          scopedSlots: { customRender: 'status' }
         },
         {
           title: '编辑人',
@@ -203,74 +191,138 @@ export default {
           scopedSlots: { customRender: 'action' }
         }
       ],
+      loadData: [
+        {
+          key:'1',
+          no:'督办单1',
+          description:'上钢新村街道',
+          callNo:'黄浦江',
+          status:'排口',
+          editor:'张三',
+          updatedAt:'2019-7-8',
+        },
+        {
+          key:'2',
+          no:'督办单1',
+          description:'上钢新村街道',
+          callNo:'黄浦江',
+          status:'排口',
+          editor:'张三',
+          updatedAt:'2019-7-8',
+        },
+      ],
+      // columns: [
+      //   {
+      //     title: '序号',
+      //     scopedSlots: { customRender: 'serial' },
+      //     sorter: true
+      //   },
+      //   {
+      //     title: '名称',
+      //     dataIndex: 'no'
+      //   },
+      //   {
+      //     title: '所属街道',
+      //     dataIndex: 'description',
+      //     scopedSlots: { customRender: 'description' }
+      //   },
+      //   {
+      //     title: '所属河道',
+      //     dataIndex: 'callNo',
+      //     sorter: true,
+      //     needTotal: true,
+      //     customRender: text => text + ' 次'
+      //   },
+      //   {
+      //     title: '风险源类型',
+      //     dataIndex: 'status',
+      //     scopedSlots: { customRender: 'status' }
+      //   },
+      //   {
+      //     title: '编辑人',
+      //     dataIndex: 'editor',
+      //     sorter: true
+      //   },
+      //   {
+      //     title: '调查日期',
+      //     dataIndex: 'updatedAt',
+      //     sorter: true
+      //   },
+      //   {
+      //     title: '操作',
+      //     dataIndex: 'action',
+      //     width: '150px',
+      //     scopedSlots: { customRender: 'action' }
+      //   }
+      // ],
       // 加载数据方法 必须为 Promise 对象
-      loadData: parameter => {
-        console.log('loadData.parameter', parameter)
-        return getServiceList(Object.assign(parameter, this.queryParam)).then(res => {
-          return res.result
-        })
-      },
+      // loadData: parameter => {
+      //   console.log('loadData.parameter', parameter)
+      //   return getServiceList(Object.assign(parameter, this.queryParam)).then(res => {
+      //     return res.result
+      //   })
+      // },
       selectedRowKeys: [],
       selectedRows: []
     }
   },
-  filters: {
-    statusFilter(type) {
-      return statusMap[type].text
-    },
-    statusTypeFilter(type) {
-      return statusMap[type].status
-    }
-  },
-  created() {
-    this.tableOption()
-    getRoleList({ t: new Date() })
-  },
+  // filters: {
+  //   statusFilter(type) {
+  //     return statusMap[type].text
+  //   },
+  //   statusTypeFilter(type) {
+  //     return statusMap[type].status
+  //   }
+  // },
+  // created() {
+  //   // this.tableOption()
+  //   getRoleList({ t: new Date() })
+  // },
   methods: {
     onChange(date, dateString) {
       console.log(date, dateString)
     },
-    tableOption() {
-      if (!this.optionAlertShow) {
-        this.options = {
-          alert: {
-            show: true,
-            clear: () => {
-              this.selectedRowKeys = []
-            }
-          },
-          rowSelection: {
-            selectedRowKeys: this.selectedRowKeys,
-            onChange: this.onSelectChange,
-            getCheckboxProps: record => ({
-              props: {
-                disabled: record.no === 'No 2', // Column configuration not to be checked
-                name: record.no
-              }
-            })
-          }
-        }
-        this.optionAlertShow = true
-      } else {
-        this.options = {
-          alert: false,
-          rowSelection: null
-        }
-        this.optionAlertShow = false
-      }
-    },
+    // tableOption() {
+    //   if (!this.optionAlertShow) {
+    //     this.options = {
+    //       alert: {
+    //         show: true,
+    //         clear: () => {
+    //           this.selectedRowKeys = []
+    //         }
+    //       },
+    //       rowSelection: {
+    //         selectedRowKeys: this.selectedRowKeys,
+    //         onChange: this.onSelectChange,
+    //         getCheckboxProps: record => ({
+    //           props: {
+    //             disabled: record.no === 'No 2', // Column configuration not to be checked
+    //             name: record.no
+    //           }
+    //         })
+    //       }
+    //     }
+    //     this.optionAlertShow = true
+    //   } else {
+    //     this.options = {
+    //       alert: false,
+    //       rowSelection: null
+    //     }
+    //     this.optionAlertShow = false
+    //   }
+    // },
 
-    handleEdit(record) {
-      console.log(record)
-      this.$refs.modal.edit(record)
-    },
-    handleSub(record) {
-      if (record.status !== 0) {
-        this.$message.info(`${record.no} 订阅成功`)
-      } else {
-        this.$message.error(`${record.no} 订阅失败，规则已关闭`)
-      }
-    },
+    // handleEdit(record) {
+    //   console.log(record)
+    //   this.$refs.modal.edit(record)
+    // },
+    // handleSub(record) {
+    //   if (record.status !== 0) {
+    //     this.$message.info(`${record.no} 订阅成功`)
+    //   } else {
+    //     this.$message.error(`${record.no} 订阅失败，规则已关闭`)
+    //   }
+    // },
     handleOk() {
       this.$refs.table.refresh()
     },
