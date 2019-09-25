3e6b5859a8a30dfbd2dc84efd259893985677167
truthiswong <1457368987@qq.com>
2019-09-25 20:18:28 +0800

    添加河道

---
+++ src/views/settings/modules/AddRiver.vue
@@ -0,0 +1,340 @@
+<template>
+  <a-modal
+    title="添加河道"
+    :width="980"
+    :visible="visible"
+    :confirmLoading="confirmLoading"
+    @ok="handleSubmit"
+    @cancel="handleCancel"
+    :mask="true"
+    :centered="true"
+    :footer="null"
+  >
+    <a-spin :spinning="confirmLoading">
+      <a-form class="from">
+        <p>基础信息</p>
+        <a-row style="width:100%">
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="河道名称">
+              <a-input placeholder="请输入河道名称" />
+            </a-form-item>
+          </a-col>
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="河道长度(m)">
+              <a-input placeholder />
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row style="width:100%">
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="河道编号">
+              <a-input placeholder />
+            </a-form-item>
+          </a-col>
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="水域面积(m²)">
+              <a-input placeholder />
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row style="width:100%">
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="管理级别" has-feedback>
+              <a-select default-value="1">
+                <a-select-option value="1">市</a-select-option>
+                <a-select-option value="2">区</a-select-option>
+                <a-select-option value="3">村</a-select-option>
+              </a-select>
+            </a-form-item>
+          </a-col>
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="是否重点" has-feedback>
+              <a-select default-value="1">
+                <a-select-option value="1">重点</a-select-option>
+                <a-select-option value="2">非重点</a-select-option>
+              </a-select>
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <p style="margin-top: 10px;">起点</p>
+        <a-row style="width:100%">
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="起点位置" has-feedback>
+              <a-cascader
+                :options="options"
+                :showSearch="{filter}"
+                @change="onChange"
+                placeholder="选择起点"
+              />
+            </a-form-item>
+          </a-col>
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="详细地址" has-feedback>
+              <a-input placeholder="请输入详细起点位置" />
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row style="width:100%">
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="经度" has-feedback>
+              <a-input placeholder />
+            </a-form-item>
+          </a-col>
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="纬度" has-feedback>
+              <a-input placeholder />
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <p style="margin-top: 10px;">终点</p>
+        <a-row style="width:100%">
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="终点位置" has-feedback>
+              <a-cascader
+                :options="options"
+                :showSearch="{filter}"
+                @change="onChange"
+                placeholder="选择起点"
+              />
+            </a-form-item>
+          </a-col>
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="详细地址" has-feedback>
+              <a-input placeholder="请输入详细起点位置" />
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row style="width:100%">
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="经度" has-feedback>
+              <a-input placeholder />
+            </a-form-item>
+          </a-col>
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="纬度" has-feedback>
+              <a-input placeholder />
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <p style="margin-top: 10px;">其他信息</p>
+        <a-row style="width:100%">
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="一级河长">
+              <a-input placeholder />
+            </a-form-item>
+          </a-col>
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="常水位(m)">
+              <a-input placeholder />
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row style="width:100%">
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="河口面积(m²)">
+              <a-input placeholder />
+            </a-form-item>
+          </a-col>
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="最高控制水位(m)">
+              <a-input placeholder />
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row style="width:100%">
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="平均水深(m)">
+              <a-input placeholder />
+            </a-form-item>
+          </a-col>
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="最大水深(m)">
+              <a-input placeholder />
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row style="width:100%">
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="河口最小面宽(m)">
+              <a-input placeholder />
+            </a-form-item>
+          </a-col>
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="河口最大面宽(m)">
+              <a-input placeholder />
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <p style="margin-top: 10px;">预设计划</p>
+        <a-row style="width:100%">
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="每月巡查次数(次)">
+              <a-input placeholder />
+            </a-form-item>
+          </a-col>
+        </a-row>
+        
+        
+      </a-form>
+      <a-divider orientation="left"></a-divider>
+      <a-row style="width:100%; margin-top:10px;" type="flex" justify="space-around">
+          <!-- <a-button-group>
+            <a-button>取消</a-button>
+            <a-button>保存</a-button>
+            <a-button>修改河道区域</a-button>
+            <a-button>关闭河道</a-button>
+          </a-button-group> -->
+          <a-col :span="3">
+            <a-button block>取消</a-button>
+          </a-col>
+          <a-col :span="3">
+            <a-button block>保存</a-button>
+          </a-col>
+          <a-col :span="3">
+            <a-button block>修改河道区域</a-button>
+          </a-col>
+          <a-col :span="3">
+            <a-button block>关闭河道</a-button>
+          </a-col>
+        </a-row>
+    </a-spin>
+  </a-modal>
+</template>
+
+<script>
+const OPTIONS = ['Apples', 'Nails', 'Bananas', 'Helicopters']
+export default {
+  data() {
+    return {
+      labelCol: {
+        xs: { span: 18 },
+        sm: { span: 6 }
+      },
+      wrapperCol: {
+        xs: { span: 18 },
+        sm: { span: 16 }
+      },
+      visible: false,
+      confirmLoading: false,
+
+      selectedItems: [], //风险源类型
+      headers: {
+        authorization: 'authorization-text'
+      },
+
+      options: [
+        {
+          value: 'zhejiang',
+          label: 'Zhejiang',
+          children: [
+            {
+              value: 'hangzhou',
+              label: 'Hangzhou',
+              children: [
+                {
+                  value: 'xihu',
+                  label: 'West Lake'
+                },
+                {
+                  value: 'xiasha',
+                  label: 'Xia Sha',
+                  disabled: true
+                }
+              ]
+            }
+          ]
+        },
+        {
+          value: 'jiangsu',
+          label: 'Jiangsu',
+          children: [
+            {
+              value: 'nanjing',
+              label: 'Nanjing',
+              children: [
+                {
+                  value: 'zhonghuamen',
+                  label: 'Zhong Hua men'
+                }
+              ]
+            }
+          ]
+        }
+      ],
+
+      form: this.$form.createForm(this)
+    }
+  },
+  computed: {
+    filteredOptions() {
+      return OPTIONS.filter(o => !this.selectedItems.includes(o))
+    }
+  },
+  methods: {
+    add() {
+      this.visible = true
+    },
+    // 添加河流
+    addRiver(value) {
+      console.log(value)
+    },
+    // 风险源类型
+    handleChange(selectedItems) {
+      this.selectedItems = selectedItems
+      console.log(selectedItems)
+    },
+    // 文件上传
+    fileUpload(info) {
+      if (info.file.status !== 'uploading') {
+        console.log(info.file, info.fileList)
+      }
+      if (info.file.status === 'done') {
+        this.$message.success(`${info.file.name} file uploaded successfully`)
+      } else if (info.file.status === 'error') {
+        this.$message.error(`${info.file.name} file upload failed.`)
+      }
+    },
+    // 标签
+    handleChangeTag(value) {
+      console.log(`selected ${value}`)
+    },
+    handleSubmit() {
+      const {
+        form: { validateFields }
+      } = this
+      this.confirmLoading = true
+      validateFields((errors, values) => {
+        if (!errors) {
+          console.log('values', values)
+          setTimeout(() => {
+            this.visible = false
+            this.confirmLoading = false
+            this.$emit('ok', values)
+          }, 1500)
+        } else {
+          this.confirmLoading = false
+        }
+      })
+    },
+    handleCancel() {
+      this.visible = false
+    },
+    // 选择地址
+    onChange(value, selectedOptions) {
+      console.log(value, selectedOptions)
+    },
+    filter(inputValue, path) {
+      return path.some(option => option.label.toLowerCase().indexOf(inputValue.toLowerCase()) > -1)
+    }
+  }
+}
+</script>
+<style scoped>
+.ant-form-item {
+  margin-bottom: 0;
+}
+.from {
+  max-height: 450px;
+  overflow-y: scroll;
+}
+</style>
