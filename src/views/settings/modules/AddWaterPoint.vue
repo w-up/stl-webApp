84fbe8cf084550a87d407d6a49df21f762253764
truthiswong <1457368987@qq.com>
2019-09-27 19:29:41 +0800

    添加水质点

---
+++ src/views/settings/modules/AddWaterPoint.vue
@@ -0,0 +1,267 @@
+<template>
+  <a-modal
+    title="添加街道"
+    :width="800"
+    :visible="visible"
+    :confirmLoading="confirmLoading"
+    @ok="handleSubmit"
+    @cancel="handleCancel"
+    :mask="true"
+    :centered="true"
+    :footer=null
+  >
+    <a-spin :spinning="confirmLoading">
+      <a-form class="from">
+        <a-row style="width:100%">
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="名称">
+              <a-input placeholder="名称" />
+            </a-form-item>
+          </a-col>
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="编号">
+              <a-input placeholder="编号" />
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row style="width:100%">
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="等级">
+              <a-input placeholder="等级" />
+            </a-form-item>
+          </a-col>
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="市考断面">
+              <a-input placeholder="市考断面" />
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row style="width:100%">
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="地址">
+              <a-input placeholder="请输入地址" />
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row style="width:100%">
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="经度">
+              <a-input readOnly placeholder="经度" />
+            </a-form-item>
+          </a-col>
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="纬度">
+              <a-input :disabled="true" placeholder="纬度" />
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row style="width:100%">
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="水体名称">
+              <a-input placeholder="等级" />
+            </a-form-item>
+          </a-col>
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="水体编码">
+              <a-input placeholder="市考断面" />
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row style="width:100%">
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="管理等级">
+              <a-input placeholder="等级" />
+            </a-form-item>
+          </a-col>
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="水体分类">
+              <a-input placeholder="市考断面" />
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row style="width:100%">
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="行政区">
+              <a-input placeholder="等级" />
+            </a-form-item>
+          </a-col>
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="所在街道">
+              <a-input placeholder="市考断面" />
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row style="width:100%">
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="所在村">
+              <a-input placeholder="等级" />
+            </a-form-item>
+          </a-col>
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="责任单位">
+              <a-input placeholder="市考断面" />
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row style="width:100%">
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="检测单位">
+              <a-input placeholder="等级" />
+            </a-form-item>
+          </a-col>
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="所属部门">
+              <a-input placeholder="市考断面" />
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row style="width:100%">
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="原检测站编码">
+              <a-input placeholder="请输入姓名" />
+            </a-form-item>
+          </a-col>
+          <a-col :span="12">
+            <a-form-item :label-col="labelCol" :wrapper-col="wrapperCol" label="备注">
+              <a-input placeholder="请输入姓名" />
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row style="width:100%; margin-top:10px;" type="flex" justify="space-around">
+          <a-col :span="4">
+            <a-button type="primary" block>取消</a-button>
+          </a-col>
+          <a-col :span="4">
+            <a-button type="primary" block>保存</a-button>
+          </a-col>
+        </a-row>
+      </a-form>
+    </a-spin>
+  </a-modal>
+</template>
+
+<script>
+const OPTIONS = ['Apples', 'Nails', 'Bananas', 'Helicopters']
+export default {
+  data() {
+    return {
+      labelCol: {
+        xs: { span: 18 },
+        sm: { span: 6 }
+      },
+      wrapperCol: {
+        xs: { span: 6 },
+        sm: { span: 18 }
+      },
+      visible: false,
+      confirmLoading: false,
+
+      selectedItems: [], //风险源类型
+      headers: {
+        authorization: 'authorization-text'
+      },
+
+      options: [{
+        value: 'zhejiang',
+        label: 'Zhejiang',
+        children: [{
+          value: 'hangzhou',
+          label: 'Hangzhou',
+          children: [{
+            value: 'xihu',
+            label: 'West Lake',
+          }, {
+            value: 'xiasha',
+            label: 'Xia Sha',
+            disabled: true,
+          }],
+        }],
+      }, {
+        value: 'jiangsu',
+        label: 'Jiangsu',
+        children: [{
+          value: 'nanjing',
+          label: 'Nanjing',
+          children: [{
+            value: 'zhonghuamen',
+            label: 'Zhong Hua men',
+          }],
+        }],
+      }],
+      
+
+      form: this.$form.createForm(this)
+    }
+  },
+  computed: {
+    filteredOptions() {
+      return OPTIONS.filter(o => !this.selectedItems.includes(o))
+    }
+  },
+  methods: {
+    add() {
+      this.visible = true
+    },
+    // 添加河流
+    addRiver(value) {
+      console.log(value)
+    },
+    // 风险源类型
+    handleChange(selectedItems) {
+      this.selectedItems = selectedItems
+      console.log(selectedItems)
+    },
+    // 文件上传
+    fileUpload(info) {
+      if (info.file.status !== 'uploading') {
+        console.log(info.file, info.fileList)
+      }
+      if (info.file.status === 'done') {
+        this.$message.success(`${info.file.name} file uploaded successfully`)
+      } else if (info.file.status === 'error') {
+        this.$message.error(`${info.file.name} file upload failed.`)
+      }
+    },
+    // 标签
+    handleChangeTag(value) {
+      console.log(`selected ${value}`)
+    },
+    handleSubmit() {
+      const {
+        form: { validateFields }
+      } = this
+      this.confirmLoading = true
+      validateFields((errors, values) => {
+        if (!errors) {
+          console.log('values', values)
+          setTimeout(() => {
+            this.visible = false
+            this.confirmLoading = false
+            this.$emit('ok', values)
+          }, 1500)
+        } else {
+          this.confirmLoading = false
+        }
+      })
+    },
+    handleCancel() {
+      this.visible = false
+    },
+    // 选择地址
+    onChange(value, selectedOptions) {
+      console.log(value, selectedOptions);
+    },
+    filter(inputValue, path) {
+      return (path.some(option => (option.label).toLowerCase().indexOf(inputValue.toLowerCase()) > -1));
+    },
+  }
+}
+</script>
+<style scoped>
+.ant-form-item {
+  margin-bottom: 0;
+}
+.from {
+  max-height: 520px;
+}
+</style>
