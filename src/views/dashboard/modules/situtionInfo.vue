3489b089c534b17092b5f9fdd3a231e0328952f6
DESKTOP-REL0A72\cyh <cyh2175@foxmail.com>
2019-09-28 18:22:45 +0800

    添加任务完成情况弹框

---
+++ src/views/dashboard/modules/situtionInfo.vue
@@ -0,0 +1,29 @@
+<template>
+     <a-modal title="任务完成情况" :visible="visible" onOk="handleOk">
+        <template slot="footer">
+            <a-button type="primary" @click="handleOk">确定</a-button>
+        </template>
+      <p>hehe</p>
+    </a-modal>
+</template>
+<script>
+export default {
+    name:'situtionInfo',
+    data(){
+        return{
+            visible:false
+        }
+    },
+    methods:{
+        show(){
+            this.visible = true;
+        },
+        handleOk(){
+            this.visible = false;
+        }
+    }
+}
+</script>
+<style lang="less" scoped>
+
+</style>
