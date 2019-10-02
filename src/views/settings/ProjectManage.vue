36d5a61b9c13dae0303ae9ad065f07f907088b1f
truthiswong <1457368987@qq.com>
2019-10-02 16:34:55 +0800

    项目管理面包屑

--- src/views/settings/ProjectManage.vue
+++ src/views/settings/ProjectManage.vue
@@ -1,21 +1,15 @@
 <template>
-  <div >
-    <a-card >
+  <div>
+    <a-card>
+      <a-breadcrumb style="font-size: 16px;">
+        <a-breadcrumb-item>设置</a-breadcrumb-item>
+        <a-breadcrumb-item>项目管理</a-breadcrumb-item>
+      </a-breadcrumb>
+      <a-divider style="margin: 10px 0" />
       <div style="display:flex;width:100%;box-sizing:border-box;">
         <div style="width:220px">
-          <!-- <div class="boder-tree"> -->
-            <!-- <a-directory-tree
-              multiple
-              defaultExpandAll
-              @select="onSelect"
-              @expand="onExpand"
-              :treeData="treeData"
-            >
-            </a-directory-tree> -->
-            <!-- <Tree :data="treeData" @on-select-change='select'></Tree>
-          </div> -->
           <a-card style="min-height: 200px">
-            <a-directory-tree :treeData="treeData"  @select='select'></a-directory-tree>
+            <a-directory-tree :treeData="treeData" @select="select"></a-directory-tree>
           </a-card>
         </div>
         <div style="width:100%;margin-left:20px">
@@ -23,51 +17,50 @@
             <a-button type="primary" icon="plus" @click="visible=true" style="margin-bottom:15px">添加</a-button>
             <a-table :columns="columns" :dataSource="data" bordered>
               <template slot="operation" slot-scope="row">
-                <a @click="handleEdit(id)">编辑</a>
+                <a @click="handleEdit(row.id)">编辑</a>
                 <a-divider type="vertical" />
-                <a @click="handleSub(id)">删除</a>
+                <a @click="handleSub(row.id)">删除</a>
               </template>
             </a-table>
           </div>
           <div v-else>
-            <a-button type="primary" icon="plus" @click="equipmentModel=true" style="margin-bottom:15px" >添加</a-button>
+            <a-button
+              type="primary"
+              icon="plus"
+              @click="equipmentModel=true"
+              style="margin-bottom:15px"
+            >添加</a-button>
             <a-table :columns="columns1" :dataSource="data1" bordered>
               <template slot="operation" slot-scope="row">
                 <a @click="equipmentModel=true">编辑</a>
                 <a-divider type="vertical" />
-                <a @click="handleSub(id)">删除</a>
+                <a @click="handleSub(row.id)">删除</a>
               </template>
             </a-table>
           </div>
         </div>
       </div>
     </a-card>
-    <a-modal
-      title=" 添加一级项目"
-      v-model="visible"
-    >
+    <a-modal title=" 添加一级项目" v-model="visible">
       <Form ref="formValidate" :model="equipmentList" :rules="ruleValidate" :label-width="90">
         <FormItem label="项目名称" prop="name">
-          <Input v-model="equipmentList.name" placeholder="请输入" style="width:200px"></Input>
+          <Input v-model="equipmentList.name" placeholder="请输入" style="width:200px"/>
         </FormItem>
       </Form>
     </a-modal>
-    <a-modal
-      title="添加二级项目"
-      v-model="equipmentModel"
-    >
+    <a-modal title="添加二级项目" v-model="equipmentModel">
       <Form ref="formValidate" :model="equipmentList" :rules="ruleValidate" :label-width="90">
         <FormItem label="上级项目" prop="name">
-          <Input v-model="equipmentList.name" placeholder="请输入" style="width:200px"></Input>
+          <Input v-model="equipmentList.name" placeholder="请输入" style="width:200px"/>
         </FormItem>
         <FormItem label="项目名称" prop="type">
-          <Input v-model="equipmentList.type" placeholder="请输入" style="width:200px"></Input>
+          <Input v-model="equipmentList.type" placeholder="请输入" style="width:200px"/>
         </FormItem>
         <FormItem label="间隔周期">
-          <a-select  style="width: 200px" placeholder="请选择" >
-            <a-select-option value="si" >日</a-select-option>
-            <a-select-option value="sis" >周</a-select-option>
-            <a-select-option value="dsi" >月</a-select-option>
+          <a-select style="width: 200px" placeholder="请选择">
+            <a-select-option value="si">日</a-select-option>
+            <a-select-option value="sis">周</a-select-option>
+            <a-select-option value="dsi">月</a-select-option>
           </a-select>
         </FormItem>
       </Form>
@@ -76,148 +69,146 @@
 </template>
 
 <script>
-const treeData = [{
-  title: '全部',
-  key: '1',
-  id:'1',
-  children: [{
-    title: '上海市',
-    key: '2-2',
-    id:'2',
+const treeData = [
+  {
+    title: '全部',
+    key: '1',
+    id: '1',
     children: [
-      { title: '黄浦区', key: '3-1',id:'3' },
-      { title: '浦东新区', key: '3-2',id:'3' },
-      { title: '静安区', key: '3-3' ,id:'3'},
-    ],
-  }, {
-    title: '四川省',
-    key: '2-3',
-    id:'2',
-    children: [
-     
-    ],
-  }],
-}]
+      {
+        title: '上海市',
+        key: '2-2',
+        id: '2',
+        children: [
+          { title: '黄浦区', key: '3-1', id: '3' },
+          { title: '浦东新区', key: '3-2', id: '3' },
+          { title: '静安区', key: '3-3', id: '3' }
+        ]
+      },
+      {
+        title: '四川省',
+        key: '2-3',
+        id: '2',
+        children: []
+      }
+    ]
+  }
+]
 const columns = [
-   {
+  {
     title: '序号',
-    width:80,
-    dataIndex: 'key',
-  }, {
+    width: 80,
+    dataIndex: 'key'
+  },
+  {
     title: '一级分类名称',
-    dataIndex: 'typeName',
-  },{
+    dataIndex: 'typeName'
+  },
+  {
     title: '操作',
     scopedSlots: { customRender: 'operation' },
-    width:120,
+    width: 120
   }
-];
+]
 const columns1 = [
-   {
+  {
     title: '序号',
-    width:80,
-    dataIndex: 'key',
-  }, {
+    width: 80,
+    dataIndex: 'key'
+  },
+  {
     title: '二级分类名称',
-    dataIndex: 'typeName',
-  },{
+    dataIndex: 'typeName'
+  },
+  {
     title: '操作',
     scopedSlots: { customRender: 'operation' },
-    width:120,
+    width: 120
   }
-];
-const data = [{
-  id:'111',
-  key: '1',
-  typeName: '上海市',
-  }, {
+]
+const data = [
+  {
+    id: '111',
+    key: '1',
+    typeName: '上海市'
+  },
+  {
     key: '2',
-    id:'222',
-    typeName: '四川省',
-}];
+    id: '222',
+    typeName: '四川省'
+  }
+]
 export default {
-  data () {
+  data() {
     return {
-      treeId:true,
+      treeId: true,
       treeData,
       columns,
       columns1,
       data,
-      visible:false,
-      equipmentModel:false,
-      typeList:{
-        id:'',
-        typeName:'',
+      visible: false,
+      equipmentModel: false,
+      typeList: {
+        id: '',
+        typeName: ''
       },
-      equipmentList:{
-        name:'',
-        type:'',
-        number:'',
+      equipmentList: {
+        name: '',
+        type: '',
+        number: ''
       },
       ruleValidate: {
-        name: [
-          { required: true, message: '不能为空', trigger: 'blur' }
-        ],
-        type: [
-          { required: true, message: '不能为空', trigger: 'blur' }
-        ],
+        name: [{ required: true, message: '不能为空', trigger: 'blur' }],
+        type: [{ required: true, message: '不能为空', trigger: 'blur' }]
       },
-      data1:[
+      data1: [
         {
-          id:'111',
+          id: '111',
           key: '1',
-          number:'2',
+          number: '2',
           typeName: '黄浦区',
-          state:true
+          state: true
         },
         {
           key: '2',
-          id:'222',
-          number:'1',
+          id: '222',
+          number: '1',
           typeName: '浦东新区',
-          state:true
+          state: true
         },
         {
           key: '3',
-          id:'333',
-          number:'4',
+          id: '333',
+          number: '4',
           typeName: '闵行区',
-          state:false
+          state: false
         }
-      ],
-
+      ]
     }
   },
-  watch: {
-
-  },
+  watch: {},
   methods: {
-    select(e){
-      var s=e[0].substr(0,1);
-      console.log(s);
-      if (s=='1') {
-        this.treeId=true
-      }else{
-        this.treeId=false
+    select(e) {
+      var s = e[0].substr(0, 1)
+      console.log(s)
+      if (s == '1') {
+        this.treeId = true
+      } else {
+        this.treeId = false
       }
     },
-    handleEdit(row){
-      this.visible=true
-      this.typeList.id=row.id
-      this.typeList.typeName=row.typeName
+    handleEdit(row) {
+      this.visible = true
+      this.typeList.id = row.id
+      this.typeList.typeName = row.typeName
     },
-    handleSub(){
-      
-    },
-
-  },
+    handleSub() {}
+  }
 }
 </script>
 
 <style lang="less" scoped>
-.boder-tree{
-  
-  border: 1px solid #b8b8b8
+.boder-tree {
+  border: 1px solid #b8b8b8;
 }
-
 </style>
