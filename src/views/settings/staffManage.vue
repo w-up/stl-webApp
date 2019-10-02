239971dd65e47821c36e58055c8b3c743e36604f
truthiswong <1457368987@qq.com>
2019-10-02 16:33:46 +0800

    员工管理面包屑

--- src/views/settings/staffManage.vue
+++ src/views/settings/staffManage.vue
@@ -1,7 +1,11 @@
 <template>
   <div  >
     <a-card >
-      <p style="color: #333; font-weight: 900;font-size: 16px;">筛选查询</p>
+      <a-breadcrumb style="font-size: 16px;">
+        <a-breadcrumb-item>设置</a-breadcrumb-item>
+        <a-breadcrumb-item>员工管理</a-breadcrumb-item>
+      </a-breadcrumb>
+      <a-divider style="margin: 10px 0" />
       <div>
         <a-form
           layout="inline"
