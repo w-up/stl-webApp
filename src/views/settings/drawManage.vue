7c71b9ba50d2fb83b944a9af29f6c341392228c7
truthiswong <1457368987@qq.com>
2019-10-02 16:35:19 +0800

    绘制类型管理面包屑

--- src/views/settings/drawManage.vue
+++ src/views/settings/drawManage.vue
@@ -1,24 +1,26 @@
 <template>
-  <div  >
-    <a-card  >
-      <a-button type="primary" icon="plus"  style="margin-bottom:15px" @click="visible=true" >添加</a-button>
+  <div>
+    <a-card>
+      <a-breadcrumb style="font-size: 16px;">
+        <a-breadcrumb-item>设置</a-breadcrumb-item>
+        <a-breadcrumb-item>绘制类型管理</a-breadcrumb-item>
+      </a-breadcrumb>
+      <a-divider style="margin: 10px 0" />
+      <a-button type="primary" icon="plus" style="margin-bottom:15px" @click="visible=true">添加</a-button>
       <a-table :columns="columns" :dataSource="data" bordered>
-        <template slot="operation" slot-scope="row" >
-            <div v-if="row.key!='001'">
-                <a @click="visible=true">编辑</a>
-                <a-divider type="vertical" />
-                <a @click="handleSub(id)">删除</a>
-            </div>
+        <template slot="operation" slot-scope="row">
+          <div v-if="row.key!='001'">
+            <a @click="visible=true">编辑</a>
+            <a-divider type="vertical" />
+            <a @click="handleSub(id)">删除</a>
+          </div>
         </template>
       </a-table>
     </a-card>
-    <a-modal
-      title="添加/编辑风险源"
-      v-model="visible"
-    >
+    <a-modal title="添加/编辑风险源" v-model="visible">
       <Form ref="formValidate" :model="list" :rules="ruleValidate" :label-width="110">
         <FormItem label="绘制类型" prop="name">
-          <Input v-model="list.name" placeholder="请输入" style="width:200px"></Input>
+          <Input v-model="list.name" placeholder="请输入" style="width:200px"/>
         </FormItem>
       </Form>
     </a-modal>
@@ -27,58 +29,52 @@
 
 <script>
 const columns = [
-   {
+  {
     title: '序号',
-    width:80,
-    dataIndex: 'key',
-  }, {
+    width: 80,
+    dataIndex: 'key'
+  },
+  {
     title: '绘制类型',
-    dataIndex: 'name',
-  },{
+    dataIndex: 'name'
+  },
+  {
     title: '操作',
     scopedSlots: { customRender: 'operation' },
-    width:160,
+    width: 160
   }
-];
+]
 export default {
-  data () {
+  data() {
     return {
-      list:{
-        name:'',
+      list: {
+        name: ''
       },
-      ruleValidate:{
-        name: [
-          { required: true, message: '不能为空', trigger: 'blur' }
-        ],
+      ruleValidate: {
+        name: [{ required: true, message: '不能为空', trigger: 'blur' }]
       },
-      visible:false,
+      visible: false,
       columns,
-      data:[
+      data: [
         {
-          key:'001',
-          name:'风险源',
+          key: '001',
+          name: '风险源'
         },
         {
-          key:'002',
-          name:'河道连通性',
+          key: '002',
+          name: '河道连通性'
         },
         {
-          key:'003',
-          name:'水陆分布',
-        },
+          key: '003',
+          name: '水陆分布'
+        }
       ]
     }
   },
-  watch: {
-
-  },
-  methods: {
-
-  },
+  watch: {},
+  methods: {}
 }
 </script>
 
 <style lang="less" scoped>
-
-  
 </style>
