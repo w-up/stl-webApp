a3a90f48cc7f20dee915aa59018d910b34b3018d
truthiswong <1457368987@qq.com>
2019-11-13 18:52:09 +0800

    图片编辑去掉cropper.js

--- src/views/supervise/modules/PhotoEdit.vue
+++ src/views/supervise/modules/PhotoEdit.vue
@@ -11,24 +11,7 @@
   >
     <a-spin :spinning="confirmLoading">
       <div class="content">
-        <!-- <canvas width="3728" height="1800" id="canvasId"></canvas> -->
-        <!-- <img id="image" src="../../../assets/loginBg.jpg" alt /> -->
-        <!-- <div class="img-container">
-          <img src="../../../assets/loginBg.jpg" alt="Picture" />
-        </div>-->
-        <div class="htmleaf-container">
-          <!-- Content -->
-          <div style="text-align:center;clear:both">
-            <!--<script src="/gg_bd_ad_720x90.js" type="text/javascript"></script>-->
-            <!-- <script src="/follow.js" type="text/javascript"></script> -->
-          </div>
-          <div class="container">
-            <div class="img-container" v-show="!clipedShow">
-              <img id="image" src="../../../assets/loginBg.jpg" alt="Picture" />
-            </div>
-            <img :src="clipedImage" v-show="clipedShow" alt />
-          </div>
-        </div>
+        <!-- <tui-image-editor :include-ui="useDefaultUI" :options="options"></tui-image-editor> -->
       </div>
       <a-divider orientation="left"></a-divider>
       <a-row style="width:100%; margin-top:10px;" type="flex" justify="space-around">
@@ -53,57 +36,33 @@
 </template>
 
 <script>
-import '../js/cropper.js'
 // import '../js/main.js'
+// import { ImageEditor } from '@toast-ui/vue-image-editor'
 export default {
   data() {
     return {
       visible: false,
       confirmLoading: false,
-      clipedShow: false, // 是否显示裁剪后的照片
-      clipedImage: '' //裁剪后的照片链接
+      useDefaultUI: true,
+      options: {
+        // for options prop
+        cssMaxWidth: 700,
+        cssMaxHeight: 500
+      }
     }
+  },
+  components: {
+    // 'tui-image-editor': ImageEditor
   },
   computed: {},
   mounted() {
-    setTimeout(() => {
-      this.$nextTick(() => {
-        this.init()
-        $('#image').cropper({
-          aspectRatio: 2 / 1,
-          viewMode: 1,
-          autoCrop: false, //关闭自动显示裁剪框
-          ready: function() {
-            $(this).cropper('crop')
-          },
-          crop: function(e) {
-            console.log(e)
-          }
-        })
-      })
-    }, 1000)
   },
   methods: {
     // 裁剪
     clipImage() {
-      console.log($('#image').cropper('getCroppedCanvas'))
-      var cas = $('#image').cropper('getCroppedCanvas')
-      var base64url = cas.toDataURL('image/jpeg')
-      cas.toBlob(function(e) {
-        console.log(e) //生成Blob的图片格式
-      })
-      console.log(base64url) //生成base64图片的格式
-      this.clipedShow = true
-      this.clipedImage = base64url
-      // $('#image').attr('src', base64url) //在body显示出canvas元素
     },
     // 上一步
     lastStep() {
-      $('#image').cropper('clear')
-      $('#image').cropper('reset')
-      $('#image').cropper('crop')
-      this.clipedShow = false
-      this.clipedImage = ''
     },
     init() {
       // var canvas = document.getElementById('canvasId')
