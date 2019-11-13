39b9c388f13467602a575fbea72ae56ac0863a20
truthiswong <1457368987@qq.com>
2019-11-13 18:51:10 +0800

    全景图弹窗暂时不要

--- src/views/supervise/modules/LookPanorama.vue
+++ src/views/supervise/modules/LookPanorama.vue
@@ -15,25 +15,7 @@
   >
     <a-spin :spinning="confirmLoading">
       <a-card class="content">
-        <!-- <krpano :xml="xml" :scene="scene" :lazy-load="true" :hooks="hooks"></krpano> -->
-        <!-- <krpano :xml="xml" style="width:500px;height:400px"></!--> -->
-        <div id="krpanoDIV" style="width:100%;height:100%;">
-          <noscript>
-            <table style="width:100%;height:100%;">
-              <tr style="vertical-align:middle;">
-                <td>
-                  <div style="text-align:center;">
-                    ERROR:
-                    <br />
-                    <br />Javascript not activated
-                    <br />
-                    <br />
-                  </div>
-                </td>
-              </tr>
-            </table>
-          </noscript>
-        </div>
+        <div id="pano"></div>
       </a-card>
       <a-divider orientation="left"></a-divider>
       <a-row style="width:100%;" type="flex" justify="space-around">
@@ -49,57 +31,53 @@
 </template>
 
 <script>
-import '../js/krpano'
-let krpano = null
-function krpano_onready_callback(krpano_interface) {
-  krpano = krpano_interface
-}
-import Krpano from 'vue-krpano'
+import PhotoSphereViewer from 'photo-sphere-viewer'
+import 'photo-sphere-viewer/dist/photo-sphere-viewer.css'
 export default {
   components: {
-    krpano: Krpano
   },
   data() {
-    const vm = this
     return {
       visible: false,
       confirmLoading: false,
-      // xml: '../js/test.xml',
-      // scene: '全景图测试',
-      // hooks: {
-      //   sceneChanged(scene) {
-      //     //event handler
-      //   }
-      // },
-      // init: ''
+      PSV: '',
+      factoryLink: require('../img/test36001.jpg'),
     }
   },
   computed: {},
   mounted: function() {
-    // embedpano({
-    //   xml: '../js/test.xml',
-    //   target: 'krpanoDIV',
-    //   html5: 'only',
-    //   consolelog: true,
-    //   passQueryParameters: true,
-    //   onready: krpano_onready_callback //回调函数，获取Krpano 对象 })
-    // })
-    // setTimeout(() => {
-    //   this.$nextTick(() => {
-    //     embedpano({
-    //       xml: '../js/test.xml',
-    //       target: 'krpanoDIV',
-    //       html5: 'only',
-    //       consolelog: true,
-    //       passQueryParameters: true,
-    //       onready: krpano_onready_callback //回调函数，获取Krpano 对象 })
-    //     })
-    //   })
-    // }, 1000)
+    // this.initPhotoSphere()
   },
   methods: {
     add() {
       this.visible = true
+    },
+    initPhotoSphere() {
+      this.PSV = PhotoSphereViewer({
+        container: 'pano',
+        // panorama: this.testLink,
+        panorama: this.factoryLink,
+        size: {
+          width: '100%',
+          height: '100%'
+        },
+        caption: '黄浦江支流',
+        time_anim: false,
+        default_long: 1.4441088145446443,
+        default_lat: 0.0800613513013615,
+        default_long: Math.PI / 2,
+        default_fov: 90,
+        sphere_correction: { pan: 30.01, tilt: 0, roll: 0 },
+        // max_fov: 100,         // 最大缩放值
+        // min_fov: 99,          // 最小缩放值
+        // default_fov: 100, // 默认缩放值，在1-179之间
+        // latitude_range: [0,0],//禁止上下滑动
+        // mousewheel: false,    // 禁止鼠标滚轮缩放
+        // navbar: false,
+        navbar: ['autorotate', 'zoom', 'markers', 'caption', 'fullscreen'],
+        theta_offset: 1000 // 旋转速度
+        // markers: this.markersData
+      })
     },
     handleSubmit() {},
     handleCancel() {
@@ -112,7 +90,12 @@ export default {
 .panorama {
   top: 50px;
   .content {
+    width: 100%;
     height: 60vh;
+    #pano {
+      width: 100%;
+      height: 100%;
+    }
   }
 }
 </style>
